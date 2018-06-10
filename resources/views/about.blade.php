28d29dafe39b50b5fbad4f454a58bf330b58ac08
ydaoud00 <ydaoud00@estudiantes.unileon.es>
2018-06-10 22:06:39 +0200

    Retoques de estilo

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -35,7 +35,7 @@
             <table>
                 <tr>
                     <td>
-                        <img class="img-responsive about-img pull-left"  src="{{url('/images/me.png')}}">
+                        <img class="img-responsive about-us-img pull-left"  src="{{url('/images/me.png')}}">
                     
                         <p>Yassine Daoudi, estudiante de cuarto curso en Ingeniería Informática en la Escuela de Ingeniería Industrial e Informática de la Universidad de León. No soy tan complicado como para huir ,ni quedarme aquí en silencio , pero no soy tan simple como para no advertir que no hay tres minutos, ni hay cien palabras que me puedan definir.<p>
                             
@@ -47,14 +47,14 @@
                     <td>
                     </td>
                     <td>  
-                        <img class="mg-responsive about-img" src="{{url('/images/app.png')}}">
+                        <img class="mg-responsive about-us-img" src="{{url('/images/app.png')}}">
                         <p> Education es una aplicación para compartir apuntes entre los estudiantes y profesores de difrentes grados educativos con el fin de incentivar mejor la educación. El proyecto surge como una propuesta para la parte práctica de la asignatura de Ingeniería de Software en la Escuela de Ingeniería Industrial e Informática de la Universidad de León. </p>                         
                     </td>
                 </tr>
 
                 <tr>
                     <td>
-                        <img class="img-responsive about-img pull-left"  src="{{url('/images/goal.png')}}">
+                        <img class="img-responsive about-us-img pull-left"  src="{{url('/images/goal.png')}}">
                         <p> El objetivo de la aplicación de Education es aportar ideas a las distintas personas que visualizarán esta propuesta de que debemos involucrarnos más en la distribución de la educación que hemos aprendido a otras personas. Otro de los objetivos también seria intentar sacar la maxima puntuación en la evaluación de la parte práctica por parte de los profesores de la asignatura. </p>    
                     </td>
                     <td>
